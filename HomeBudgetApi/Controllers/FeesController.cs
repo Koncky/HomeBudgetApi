81f5752b7eafee09e1f151cd7c32959d9b195fc4
Piotr Dobrowolski <piodobrowolski@gmail.com>
2022-03-07 21:35:05 +0100

    Initial commit

---
+++ HomeBudgetApi/Controllers/FeesController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HomeBudgetApi.Data;
+using HomeBudgetApi.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeBudgetApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeesController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public FeesController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/fees
+        [HttpGet]
+        public ActionResult<IEnumerable<Fee>> Get()
+        {
+            //Include - loading relationship data from another entity
+            List<Fee> fees = _context.Fee.Include(x => x.Payments).ToList();
+
+            return fees;
+        }
+
+        // GET api/fees/5
+        [HttpGet("{id}")]
+        public ActionResult<Fee> Get(int id)
+        {
+            return _context.Fee.Where(f => f.Id == id)?.Include(i => i.Payments).First();
+        }
+
+        // POST api/fees
+        [HttpPost]
+        public ActionResult<Fee> Post([FromBody] Fee fee)
+        {
+            if(fee == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Add<Fee>(fee);
+            _context.SaveChanges();
+
+            return fee;
+        }
+
+        // PUT api/fees/5
+        [HttpPut("{id}")]
+        public ActionResult<Fee> Put(int id, [FromBody] Fee fee)
+        {
+            if(id != fee.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Update<Fee>(fee);
+            _context.SaveChanges();
+
+            return fee;
+        }
+
+        // DELETE api/fees/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Fee feeToRemove = _context.Fee.FirstOrDefault(f => f.Id == id);
+
+            if(feeToRemove != null)
+            {
+                _context.Remove<Fee>(feeToRemove);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ HomeBudgetApi/Startup.cs
@@ -0,0 +1,68 @@
+﻿using HomeBudgetApi.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace HomeBudgetApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DatabaseContext>(opt => 
+                opt
+                    .UseMySQL(Configuration.GetConnectionString("Default")));
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy(name: MyAllowSpecificOrigins,
+                    builder =>
+                    {
+                        builder.WithOrigins("http://localhost:4200");
+                        builder.AllowAnyMethod();
+                        builder.AllowAnyHeader();
+                        builder.AllowAnyOrigin();
+                    });
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddMvc(opt => opt.EnableEndpointRouting = false);
+
+            services.AddControllers().AddNewtonsoftJson(opt =>
+                opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            
+            app.UseRouting();
+            app.UseCors(MyAllowSpecificOrigins);
+            app.UseMvc();
+        }
+    }
+}
